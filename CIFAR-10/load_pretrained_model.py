86605dd77eb6c8224e27522ac5e96bfcfcb80bd1
bottlesteel97 <41908146+BottleChrome@users.noreply.github.com>
2021-01-18 13:35:19 +0900

    HotFix : ResNet code

--- CIFAR-10/load_pretrained_model.py
+++ CIFAR-10/load_pretrained_model.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
+
 from torchvision import datasets, transforms
 
 if torch.cuda.is_available():
@@ -55,120 +57,13 @@ for i in range(10):
     plt.imshow(np.transpose(X_train[i], (1, 2, 0)))
     plt.title('Class: ' + str(y_train[i].item()))
 
-class BasicBlock(nn.Module):
-    def __init__(self, in_planes, planes, stride = 1):
-        super(BasicBlock,self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes,
-            kernel_size= 3,
-            stride = stride,
-            padding= 1,
-            bias = False
-
-        )
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(
-            planes, planes, 
-            kernel_size= 3,
-            stride = 1,
-            padding = 1,
-            bias = False
-        )
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.shortcut = nn.Sequential( )
-        if stride != 1 or in_planes != planes :
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_planes, planes,
-                    kernel_size= 1,
-                    stride = stride,
-                    bias = False
-                ),
-                nn.BatchNorm2d(planes)
-            )
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out 
-class ResNet(nn.Module):
-    def __init__(self, num_classes = 10):
-        super(ResNet,self).__init__()
-        self.in_plane = 16
-        self.conv1 = nn.Conv2d(
-            3, 16,
-            kernel_size= 3,
-            stride= 1,
-            padding= 1,
-            bias= 1
-        )
-        self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(16,2,stride=1)
-        self.layer2 = self._make_layer(32,2,stride=2)
-        self.layer3 = self._make_layer(64,2,stride=2)
-        self.linear = nn.Linear(64, num_classes)
-
-    def _make_layer(self, planes, num_blocks, stride):
-        strides = [stride] +[1] * (num_blocks-1)
-        layers = []
-        for stride in strides :
-            layers.append(BasicBlock(self.in_plane, planes, stride))
-            self.in_plane = planes
-        return nn.Sequential(*layers)
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = F.avg_pool2d(out, 8)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out 
 
 
+model = models.resnet34(pretrained=False)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 10)
+model = model.cuda()
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN,self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_channels=3,
-            out_channels=8,
-            kernel_size=3,
-            padding=1
-        )
-        self.conv2 = nn.Conv2d(
-            in_channels= 8 ,
-            out_channels= 16,
-            kernel_size= 3,
-            padding= 1
-
-        )
-        self.pool = nn.MaxPool2d(kernel_size=2,
-        stride = 2)
-
-        self.fc1 = nn.Linear(8*8*16, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32,10)
-    def forward(self,x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.pool(x)
-
-        x = x.view(-1, 8*8*16)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.fc3(x)
-        x = F.log_softmax(x)
-        return x
-
-
-model = ResNet().to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 criterion = nn.CrossEntropyLoss()
 
@@ -218,3 +113,16 @@ for epoch in range(1, EPOCHS + 1):
         epoch, test_loss, test_accuracy))
 
 
+
+model = models.resnet34(pretrained=True)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 10)
+model = model.cuda()
+
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+criterion = nn.CrossEntropyLoss()
+for epoch in range(1, EPOCHS + 1):
+    train(model, train_loader, optimizer, log_interval = 200)
+    test_loss, test_accuracy = evaluate(model, test_loader)
+    print("\n[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f} % \n".format(
+        epoch, test_loss, test_accuracy))
