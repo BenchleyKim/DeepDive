d2dc9045307bc1900e100e725da7abebf96efd4b
bottlesteel97 <41908146+BottleChrome@users.noreply.github.com>
2021-01-18 14:35:20 +0900

    Update Base.py

--- NLP_tasks/Base.py
+++ NLP_tasks/Base.py
@@ -1,1 +1,7 @@
 from torchtext import data
+from torchtext import datasets
+
+TEXT = data.Field(lower=True, batch_first=True)
+LABEL = data.Field(sequential=False)
+
+train, test = datasets.IMDB.splits(TEXT, LABEL)
